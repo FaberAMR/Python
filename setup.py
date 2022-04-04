2b843ddf7d324b23309e3d4748941bcb699d4ecf
Fabri Guiffrey <amrcdelu@gmail.com>
2022-04-04 20:23:07 -0300

    este es el primer comentario

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup       
+
+setup(
+    name="paquete_fabri", 
+    version="1.0",
+    description="Estamos haciendo un paquete",
+    author="Fabricio Guiffrey",
+    author_email="amrcdelu@gmail.com",
+
+    packages= ["paquete_fabri"]
+)
